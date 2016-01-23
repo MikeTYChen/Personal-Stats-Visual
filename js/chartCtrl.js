a7da21d7db8319e06f4f034ecd8d800421c48ff4
Michael Chen <miketychen@gmail.com>
2016-01-23 15:23:03 -0600

    data

--- js/chartCtrl.js
+++ js/chartCtrl.js
@@ -3,7 +3,7 @@ angular.module('chartCtrl', [])
   .config(['ChartJsProvider', function (ChartJsProvider) {
     // Configure all charts
     ChartJsProvider.setOptions({
-      colours: ['#FF5252', '#FF8A80'],
+      colours: ['#FF5252', '#3df'],
 
       responsive: true
     });
@@ -14,23 +14,27 @@ angular.module('chartCtrl', [])
   }])
   .controller("chartController", ['$scope', '$timeout', '$http', function ($scope, $timeout,$http) {
     workoutData = [];
+    timeData = [];
     $scope.lastUpdate = "";
     $http.get('data/data.json').success(function(data) {
       tempMiles = [];
+      tempTime = [];
       tempDate = [];
       var howMany = 24; // show only 14 days;
       $scope.lastUpdate = data[0].lastUpdate;
       for(var num = 1; num<data.length;num++){
         tempMiles.push(parseInt(data[num].mile));
+        tempTime.push(parseInt(data[num].time));
         tempDate.push(data[num].date);
       }
       $scope.phones = data[0];
       workoutData = tempMiles;
+      timeData = tempTime;
       $scope.labels = tempDate;
       var length = workoutData.length;
       $scope.labels = tempDate.slice(length-howMany, length);
-      $scope.data = [workoutData.slice(length-howMany, length)];
+      $scope.data = [workoutData.slice(length-howMany, length),timeData.slice(length-howMany, length)];
     });
-  $scope.series = ['Workouts'];
+  $scope.series = ['Miles','Minutes'];
 
 }]);
